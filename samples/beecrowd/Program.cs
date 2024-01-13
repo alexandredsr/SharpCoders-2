d09dba49d8fa046e1ae900f9888b57434a372cf6
xdsribeiro@gmail.com <xdsribeiro@gmail.com>
2024-01-13 12:44:03 -0300

    "Atualizacao da minha branch com exemplos do beecrowd"

---
+++ samples/beecrowd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace beecrowd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Console.ReadLine() é uma funcao que naturalmente retorna uma string
+            //adicionando comentario para testar
+            //e-nésima tentativa de atualizar minha branch
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"X = {A + B}");
+        }
+    }
+}
