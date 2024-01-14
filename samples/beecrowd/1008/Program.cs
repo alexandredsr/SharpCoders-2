a3bfd34cdf7d18ee8075e6db6fb31bb9e4bb6ab2
xdsribeiro@gmail.com <xdsribeiro@gmail.com>
2024-01-14 12:27:22 -0300

    "Adicionando alguns exemplos de C# do beecrowd"

---
+++ samples/beecrowd/1008/Program.cs
@@ -0,0 +1,13 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = Convert.ToInt32(Console.ReadLine());
+        int horas = Convert.ToInt32(Console.ReadLine());
+        double s = Math.Round(Convert.ToDouble(Console.ReadLine()), 2);
+    
+        Console.WriteLine($"NUMBER = {numero}");
+        Console.WriteLine($"SALARY = U$ {(horas * s).ToString("0.00")}");
+    }
+}
---
+++ samples/beecrowd/1042/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o valor de 'a': ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de 'b': ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de 'c': ");
+        int c = int.Parse(Console.ReadLine());
+
+        if (a > b && a > c) {
+            Console.WriteLine("Maior");
+        }
+        else if (!(a > b) && a > c || a > b && !(a > c)) {
+            Console.WriteLine("Medio");
+        }
+        else {
+            Console.WriteLine("Menor");
+        }
+
+        if (b > a && b > c) {
+            Console.WriteLine("Maior");
+        }
+        else if (!(b > a) && b > c || b > a && !(b > c)) {
+            Console.WriteLine("Medio");
+        }
+        else {
+            Console.WriteLine("Menor");
+        }
+
+        if (c > b && c > a) {
+            Console.WriteLine("Maior");
+        }
+        else if (!(c > b) && c > a || c > b && !(c > a)) {
+            Console.WriteLine("Medio");
+        }
+        else {
+            Console.WriteLine("Menor");
+        }
+
+
+        
+    }
+
+}
